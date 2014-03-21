fea61fa8466c39d62db2e756b43bd1ebfd536d2f
gmamaladze@gmail.com <gmamaladze@gmail.com>
2014-03-21 17:48:02 +0100

    Refactored Main to be the factory for different game variants.

--- TicTacToe/src/crosscutting/Main.java
+++ TicTacToe/src/crosscutting/Main.java
@@ -4,24 +4,79 @@ import java.util.Scanner;
 
 import logic.*;
 import model.*;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import ui.*;
 
-import ui.ConsolePresenter;
-
+/**
+ * @author George Mamaladze
+ * This is a launcher class for the application.
+ * It responsible for object graph creation and starting the game.
+ */
 public class Main {
 
+	/**
+	 * Starts a game variant based on console arguments.
+	 * Wrong argument prints help.
+	 * Missing argument starts the default console game.
+	 */	
 	public static void main(String[] args) {
+		if (args.length==0) {
+			playConsole();
+		}
+		
+		String arg0 = args[0];
+		switch(arg0) {
+			case "console":
+			case "c":
+				playConsole();
+				break;
+				
+			case "gui":
+			case "g":
+				playGui();
+				break;				
+				
+			default:
+				printHelp(arg0);
+				break;
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private static void printHelp(String arg0) {
+		System.out.println(String.format("Invalid argument '%s'", arg0));
+		System.out.println("Please specify on of the following arguments:");
+		System.out.println("-console / -c : to start the console game variant");
+		System.out.println("-gui / -g : to start the gui game variant");
+		//TODO: Please add help for options starting further game variants here
+	}
+
+	/**
+	 * Constructs and starts the console game
+	 */
+	private static void playConsole() {
 		Scanner scanner = new Scanner(System.in);
-		
-		Presenter presenter = new ConsolePresenter(System.out);
-		Board board = new Board();
-		Player xPlayer = new ConsolePlayer(scanner, System.out);
-		Player oPlayer = new ConsolePlayer(scanner, System.out);
-		Referee referee = new PrimeReferee();
-		
-		Game game = new Game(board, xPlayer, oPlayer, referee, presenter);
-		game.play(PlayerColor.X);
-		
-		scanner.close();
+
+		try {
+			Presenter presenter = new ConsolePresenter(System.out);
+			Board board = new Board();
+			Player xPlayer = new ConsolePlayer(scanner, System.out);
+			Player oPlayer = new ConsolePlayer(scanner, System.out);
+			Referee referee = new PrimeReferee();
+			
+			Game game = new Game(board, xPlayer, oPlayer, referee, presenter);
+			game.play(PlayerColor.X);
+		} finally {
+			scanner.close();
+		}
+	}
+	
+	/**
+	 * Constructs and starts the GUI game
+	 */
+	private static void playGui() {
+		throw new NotImplementedException();
 	}
 }
