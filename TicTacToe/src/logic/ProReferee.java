a38da9827aa02be2a26c659f3e2c88fe8a70778a
defaultcoder <defaultcdr@gmail.com>
2014-04-09 15:25:53 +0400

    #6 Created ProReferee for 32x32 board

---
+++ TicTacToe/src/logic/ProReferee.java
@@ -0,0 +1,13 @@
+package logic;
+
+import model.ReadOnlyBoard;
+
+public class ProReferee implements Referee{
+
+	@Override
+	public GameStatus getGameStatus(ReadOnlyBoard board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
